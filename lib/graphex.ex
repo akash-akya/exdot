928a71b2f99d3b0ed7e36c8b395270a5319d1bc0
akash-akya <akashh246@gmail.com>
2021-02-27 15:29:38 +0530

    Basic implementation

--- lib/graphex.ex
+++ lib/graphex.ex
@@ -1,18 +1,142 @@
+defmodule Graphex.Utils do
+  def block_to_list({:__block__, _meta, block}), do: block
+  def block_to_list(ast), do: [ast]
+
+  def indent_str(body, indentation) do
+    String.trim(body)
+    |> String.split("\n")
+    |> Enum.join("\n" <> indentation)
+  end
+
+  def indent(lines, joiner, indentation \\ "  ") when is_list(lines) do
+    Enum.join(lines, joiner) |> indent_str(indentation)
+  end
+
+  def dquote(str) when is_binary(str), do: inspect(str)
+
+  # grammar
+
+  defp fmt_value("<" <> _ = html_label), do: html_label
+  defp fmt_value(v) when is_binary(v), do: inspect(v)
+  defp fmt_value(v) when is_atom(v), do: inspect(v)
+  defp fmt_value(v), do: to_string(v)
+
+  defp fmt_kv({k, v}), do: "#{k} = #{fmt_value(v)}"
+
+  def attr_list(attrs) do
+    attrs_str = Enum.map(attrs, &fmt_kv/1) |> indent(" ")
+    " [ #{attrs_str} ] "
+  end
+
+  def fmt_body([{name, attrs}]) when is_atom(name) and is_list(attrs) do
+    to_string(name) <> attr_list(attrs)
+  end
+
+  def fmt_body([name, attrs]) when is_atom(name) and is_list(attrs) do
+    to_string(name) <> attr_list(attrs)
+  end
+
+  def fmt_body([{name, value}]) when is_atom(name) and not is_list(value) do
+    "#{name} = #{value}"
+  end
+
+  def fmt_body([name, value]) when is_atom(name) and not is_list(value) do
+    "#{name} = #{value}"
+  end
+
+  def fmt_body([key, attrs]) when is_binary(key) and is_list(attrs) do
+    key <> attr_list(attrs)
+  end
+
+  def fmt_body(stmt) when is_binary(stmt), do: stmt
+
+  def fmt_body({func, _, args} = stmt) when is_atom(func) and is_list(args), do: stmt
+
+  def normalize(v), do: v
+
+  def block(type, name, stmts) do
+    body =
+      stmts
+      |> Enum.map(&normalize/1)
+      |> Enum.map(&fmt_body/1)
+      |> indent("\n")
+
+    """
+    #{type} #{name} {
+      #{body}
+    }
+    """
+  end
+end
+
+defmodule Graphex.Html do
+  import Graphex.Utils
+
+  defp fmt_attrs(attrs) do
+    for {k, v} <- attrs, do: "#{k}=#{inspect(v)} "
+  end
+
+  defp fmt_html_node(name, attrs, body) do
+    name = String.upcase(to_string(name))
+
+    """
+    <#{name} #{fmt_attrs(attrs)}>#{body}</#{name}>
+    """
+    |> indent_str("  ")
+  end
+
+  def fmt_html(node) when is_binary(node), do: node
+  def fmt_html(nodes) when is_list(nodes), do: Enum.map(nodes, &fmt_html/1) |> indent("\n")
+
+  def fmt_html(node) when is_map(node) do
+    [{key, {attrs, body}}] = Map.to_list(node)
+    fmt_html_node(key, attrs, fmt_html(body))
+  end
+end
+
 defmodule Graphex do
-  @moduledoc """
-  Documentation for Graphex.
-  """
+  import Graphex.Utils
+  import Graphex.Html
 
-  @doc """
-  Hello world.
+  defp fmt_value("<" <> _ = html_label), do: html_label
+  defp fmt_value(v) when is_binary(v), do: inspect(v)
+  defp fmt_value(v) when is_atom(v), do: inspect(v)
+  defp fmt_value(v), do: to_string(v)
 
-  ## Examples
+  defp fmt_kv({k, v}), do: "#{k} = #{fmt_value(v)}"
 
-      iex> Graphex.hello()
-      :world
+  defp fmt_attrs(attrs) do
+    Enum.map(attrs, &fmt_kv/1) |> indent(" ")
+  end
 
-  """
-  def hello do
-    :world
+  defp stmt(name, []), do: to_string(name)
+  defp stmt(name, attrs), do: to_string(name) <> " [ " <> fmt_attrs(attrs) <> " ]"
+
+  for keyword <- [:graph, :node, :edge] do
+    def unquote(String.to_atom("#{keyword}_attr"))(attrs), do: stmt(unquote(keyword), attrs)
+  end
+
+  def defnode(name, attrs), do: stmt(inspect(name), attrs)
+
+  def defedge({a, hp}, {b, tp}, attrs),
+    do: stmt("#{dquote(a)}:#{dquote(hp)} -> #{dquote(b)}:#{dquote(tp)}", attrs)
+
+  def html_lable(body), do: "<#{fmt_html(body)}>"
+
+  for node <- [:table, :tr, :td, :b, :u, :i, :th, :font, :br, :hr] do
+    def unquote(node)(body), do: %{unquote(node) => {[], body}}
+    def unquote(node)(attrs, body), do: %{unquote(node) => {attrs, body}}
+  end
+
+  defmacro digraph(name \\ "", do: body) do
+    quote do: block("digraph", unquote(name), unquote(block_to_list(body)))
+  end
+
+  defmacro subgraph(name \\ "", do: body) do
+    quote do: block("subgraph", unquote(name), unquote(block_to_list(body)))
+  end
+
+  defmacro graph(name \\ "", do: body) do
+    quote do: block("graph", unquote(name), unquote(block_to_list(body)))
   end
 end
--- test/graphex_test.exs
+++ test/graphex_test.exs
@@ -1,8 +1,264 @@
 defmodule GraphexTest do
   use ExUnit.Case
-  doctest Graphex
+  require Graphex
+  import Graphex
 
-  test "greets the world" do
-    assert Graphex.hello() == :world
+  defp row({:primary_key, item}, index) do
+    tr(
+      td(
+        [port: "f#{index}", align: "LEFT"],
+        u(font([face: "Helvetica"], item))
+      )
+    )
+  end
+
+  defp row({:foreign_key, item}, index) do
+    tr(
+      td(
+        [port: "f#{index}", align: "LEFT"],
+        i(u(font([face: "Helvetica"], item)))
+      )
+    )
+  end
+
+  defp row(item, index) when is_binary(item) do
+    tr(
+      td(
+        [port: "f#{index}", align: "LEFT"],
+        font([face: "Helvetica"], item)
+      )
+    )
+  end
+
+  defp rows(items) do
+    for {item, i} <- Enum.with_index(items), do: row(item, i)
+  end
+
+  defp record(name, items, color, attrs) do
+    rows = [
+      th(
+        td(
+          b(
+            font(
+              ["point-size": "16", face: "Helvetica"],
+              name
+            )
+          )
+        )
+      )
+      | rows(items)
+    ]
+
+    table =
+      table(
+        [
+          bgcolor: color,
+          border: "0",
+          cellborder: "1",
+          cellpadding: "4",
+          cellspacing: "0"
+        ],
+        rows
+      )
+
+    defnode(name, Keyword.merge([label: html_lable(table), shape: "none"], attrs))
+  end
+
+  # test "digraph" do
+  #   g =
+  #     digraph do
+  #       graph_attr(rankdir: "LR", labeljust: "jleft", splines: "spline", labelloc: "vtop")
+  #       node_attr(rankdir: "TB", shape: "plaintext")
+  #       edge_attr(fontname: "Helvetica")
+
+  #       "ranksep=2"
+
+  #       record(
+  #         "Configs",
+  #         [
+  #           {:primary_key, "name [varchar, not null]"},
+  #           "schema [text, not null]",
+  #           "namespace [varchar, not null]",
+  #           "cluster [varchar, not null]"
+  #         ],
+  #         "#d0e0d0",
+  #         shape: "none"
+  #       )
+
+  #       record(
+  #         "Revisions",
+  #         [
+  #           {:primary_key, "revision_id [varchar, not null]"},
+  #           "value [text, not null]",
+  #           "metadata [text]",
+  #           "created_at [datetime, not null]",
+  #           {:foreign_key, "config_name [varchar, not null]"}
+  #         ],
+  #         "#ececfc",
+  #         shape: "none"
+  #       )
+
+  #       record(
+  #         "Tags",
+  #         [
+  #           {:primary_key, "id [bigint, not null]"},
+  #           "name [varchar, not null]",
+  #           {:foreign_key, "config_name [varchar, not null]"}
+  #         ],
+  #         "#fcecec",
+  #         shape: "none"
+  #       )
+
+  #       defedge({"Revisions", "f4"}, {"Configs", "f0"},
+  #         arrowhead: "none",
+  #         style: "dashed",
+  #         headlabel: "   1",
+  #         taillabel: "   0..N"
+  #       )
+
+  #       defedge({"Tags", "f2"}, {"Configs", "f0"},
+  #         arrowhead: "none",
+  #         style: "dashed",
+  #         headlabel: "   1",
+  #         taillabel: "   0..N"
+  #       )
+  #     end
+
+  #   IO.puts("Ouput::")
+  #   # IO.puts(Enum.join(String.split(g, "\n")))
+  #   IO.puts(g)
+
+  #   # assert """
+  #   #        digraph ERD {
+  #   #          node_attr [ randir = "TB" ]
+  #   #          graph_attr [ randir = "TB" ]
+  #   #          edge_attr [ randir = "LR" ]
+  #   #          Dept [ label = <<table bgcolor="#d0e0d0" border="0">
+  #   #            <tr >
+  #   #              <td >
+  #   #                dname
+  #   #              </td>
+  #   #            </tr>
+  #   #            <tr >
+  #   #              <td >
+  #   #                deptno
+  #   #              </td>
+  #   #            </tr>
+  #   #          </table>> shape = "none" ]
+  #   #        }
+  #   #        """ == g
+  # end
+
+  defp create_rec(name, items, color) do
+    rows = [
+      th(
+        td(
+          b(
+            font(
+              ["point-size": "16", face: "Helvetica"],
+              name
+            )
+          )
+        )
+      )
+      | rows(items)
+    ]
+
+    table =
+      table(
+        [
+          bgcolor: color,
+          border: "0",
+          cellborder: "1",
+          cellpadding: "4",
+          cellspacing: "0"
+        ],
+        rows
+      )
+
+    [name, [label: html_lable(table), shape: "none"]]
+  end
+
+  test "new graph" do
+    g =
+      digraph do
+        [graph: [rankdir: "LR", labeljust: "jleft", splines: "spline", labelloc: "vtop"]]
+        [node: [rankdir: "TB", shape: "plaintext"]]
+        [edge: [fontname: "Helvetica"]]
+
+        "ranksep=2"
+
+        subgraph "cluster_0" do
+          [style: "filled"]
+          [color: "lightgrey"]
+          [node: [style: "filled", color: "white"]]
+          "a0 -> a1 -> a2 -> a3"
+          [label: "Hello"]
+        end
+
+        """
+        subgraph cluster_1 {
+          node [style=filled];
+          b0 -> b1 -> b2 -> b3;
+          label = "World";
+          color=blue
+        }
+        """
+
+        create_rec(
+          "Configs",
+          [
+            {:primary_key, "name [varchar, not null]"},
+            "schema [text, not null]",
+            "namespace [varchar, not null]",
+            "cluster [varchar, not null]"
+          ],
+          "#d0e0d0"
+        )
+
+        create_rec(
+          "Revisions",
+          [
+            {:primary_key, "revision_id [varchar, not null]"},
+            "value [text, not null]",
+            "metadata [text]",
+            "created_at [datetime, not null]",
+            {:foreign_key, "config_name [varchar, not null]"}
+          ],
+          "#ececfc"
+        )
+
+        create_rec(
+          "Tags",
+          [
+            {:primary_key, "id [bigint, not null]"},
+            "name [varchar, not null]",
+            {:foreign_key, "config_name [varchar, not null]"}
+          ],
+          "#fcecec"
+        )
+
+        [
+          "Revisions:f4 -> Configs:f0": [
+            arrowhead: "none",
+            style: "dashed",
+            headlabel: "   1",
+            taillabel: "   0..N"
+          ]
+        ]
+
+        [
+          "Tags:f2 -> Configs:f0": [
+            arrowhead: "none",
+            style: "dashed",
+            headlabel: "   1",
+            taillabel: "   0..N"
+          ]
+        ]
+      end
+
+    IO.puts("Ouput::")
+    # IO.puts(Enum.join(String.split(g, "\n")))
+    IO.puts(g)
   end
 end
